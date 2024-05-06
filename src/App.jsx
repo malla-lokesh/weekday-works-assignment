589a407b0aec06d238326a6882af1236d527e873
malla-lokesh <121810307046@gitam.in>
2024-05-06 14:52:03 +0530

    created a job card and UI folder

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,10 @@ import {
   MenuItem,
   TextField,
 } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
 
 import "./App.css";
 import { useState } from "react";
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+import JobPost from "./Components/JobPost";
 
 function App() {
   const [roles, setRoles] = useState("");
@@ -124,30 +115,7 @@ function App() {
           </Grid>
         </Grid>
         <Grid item container spacing={2}>
-          <Grid item xs={12} sm={6} lg={4}>
-            <Item>xs = 8</Item>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={4}>
-            <Item>xs = 8</Item>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={4}>
-            <Item>xs = 8</Item>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={4}>
-            <Item>xs = 8</Item>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={4}>
-            <Item>xs = 8</Item>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={4}>
-            <Item>xs = 8</Item>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={4}>
-            <Item>xs = 8</Item>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={4}>
-            <Item>xs = 8</Item>
-          </Grid>
+          <JobPost />
         </Grid>
       </Grid>
     </Box>
---
+++ src/Components/JobPost.jsx
@@ -0,0 +1,24 @@
+import { Grid } from "@mui/material";
+import Item from "../UI/Item";
+import React from "react";
+
+const JobPost = () => {
+  return (
+    <React.Fragment>
+      <Grid item xs={12} sm={6} lg={4}>
+        <Item>xs = 8</Item>
+      </Grid>
+      <Grid item xs={12} sm={6} lg={4}>
+        <Item>xs = 8</Item>
+      </Grid>
+      <Grid item xs={12} sm={6} lg={4}>
+        <Item>xs = 8</Item>
+      </Grid>
+      <Grid item xs={12} sm={6} lg={4}>
+        <Item>xs = 8</Item>
+      </Grid>
+    </React.Fragment>
+  );
+};
+
+export default JobPost;
